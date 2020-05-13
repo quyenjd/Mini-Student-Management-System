867c8c3be047abb44f58736f12798e67be454cac
anhoangpham2001 <54526529+anhoangpham2001@users.noreply.github.com>
2020-05-13 21:07:07 +0700

    add namespace classes (An)

---
+++ include/class.cpp
@@ -0,0 +1,40 @@
+#include <cstdio>
+#include <iostream>
+#include "class.h"
+
+namespace classes{
+
+void importStudent(){
+    using namespace Csv;
+    csv_handler student("19APCS1-Student.csv");
+    if (student.init_read()==false){
+        std::cout<<"Error!";
+        return;
+    }
+    student.read_and_terminate();
+}
+
+void addStudent(){
+    using namespace Csv;
+    csv_handler student("19APCS1-Student.csv");
+    if (student.init_read()==false){
+        std::cout<<"Error!";
+        return;
+    }
+    student.get_table().add_row();
+}
+
+void deleteStudent(){
+    using namespace Csv;
+    csv_handler student("19APCS1-Student.csv");
+    if (student.init_read()==false){
+        std::cout<<"Error!";
+        return;
+    }
+    std::cout<<"Input id: ";
+    multitype id;
+    //declare multitype
+    student.get_table().get_row_where("Student ID",id);
+    student.get_table().rm_row(n);
+}
+}
---
+++ include/class.h
@@ -0,0 +1,11 @@
+#include <cstdio>
+#ifndef FUNCTION_H
+#define FUNCTION_H
+#endif // FUNCTION_H
+#include "csv.h"
+
+namespace classes{
+    void importStudent();
+    void addStudent();
+    void deleteStudent();
+}
