7a94836864683f1dee6ac33a18a100d5fffe7dbb
anhoangpham2001 <54526529+anhoangpham2001@users.noreply.github.com>
2020-06-03 12:06:04 +0700

    Add menu to Class.cpp

--- include/class.cpp
+++ include/class.cpp
@@ -7,83 +7,86 @@ namespace classes{
 
 void removeStudent(){
     using namespace Csv;
-    csv_handler student("students.csv");
-    if (student.init_read()==false){
+    csv_handler students("students.csv");
+    if (students.init_read()==false){
         std::cout<<"Error!";
         return;
     }
-    student.read_and_terminate(); //read the csv file and copy to table
-    int id;
-    std::cout<<"Insert ID of the student you want to remove: ";
-    std::cin>>id;
-    std::cin.ignore();
-    student.get_table().rm_row_where("Student ID",id);
-    if (student.init_write()==false){
+    students.read_and_terminate(); //read the csv file and copy to table
+    Interface::input_menu menu;
+    menu.set_title("Delete Student");
+    menu.add_item("Student ID: ",false);
+    list<multitype> res=menu.print_menu_and_wait(false);
+    students.get_table().rm_row_where("Student ID",res.at(0));
+    if (students.init_write()==false){
         std::cout<<"Error!";
         return;
     }
-    student.write_and_terminate(); //paste the table to the csv file
-    csv_handler idpass("idpass.csv");
-    if (idpass.init_read()==false){
+    students.write_and_terminate(); //paste the table to the csv file
+    csv_handler users("users.csv");
+    if (users.init_read()==false){
         std::cout<<"Error!";
         return;
     }
-    idpass.read_and_terminate();
-    idpass.get_table().rm_row_where("ID",id);
-    if (idpass.init_write()==false){
+    users.read_and_terminate();
+    users.get_table().rm_row_where("ID",res.at(0));
+    if (users.init_write()==false){
         std::cout<<"Error!";
         return;
     }
-    idpass.write_and_terminate();
+    users.write_and_terminate();
+    csv_handler coursestudent("coursestudent.csv");
+    if (coursestudent.init_read()==false){
+        std::cout<<"Error!";
+        return;
+    }
+    coursestudent.read_and_terminate();
+    coursestudent.get_table().rm_row_where("Student ID",res.at(0));
+    if (coursestudent.init_write()==false){
+        std::cout<<"Error!";
+        return;
+    }
+    coursestudent.write_and_terminate();
 }
 
 void addStudent(){
     using namespace Csv;
-    csv_handler student("students.csv");
-    if (student.init_read()==false){
+    csv_handler students("students.csv");
+    if (students.init_read()==false){
         std::cout<<"Error!";
         return;
     }
-    student.read_and_terminate(); //read the csv file and copy to table
-    student.get_table().add_row(); //add row to the table
+    students.read_and_terminate(); //read the csv file and copy to table
+    students.get_table().add_row(); //add row to the table
     //add data to the row
-    int id;
-    std::cout<<"Insert Student ID: ";
-    std::cin>>id;
-    student.get_table().get(student.get_table().num_rows()-1,"Student ID").assign(id);
-    char* fullname=new char[30];
-    std::cout<<"Insert fullname: ";
-    std::cin.ignore();
-    std::cin.get(fullname,'/n');
-    student.get_table().get(student.get_table().num_rows()-1,"Fullname").assign(fullname);
-    char* dob=new char[30];
-    std::cout<<"Insert date of birth: ";
-    std::cin.ignore();
-    std::cin.get(dob,'/n');
-    student.get_table().get(student.get_table().num_rows()-1,"DoB").assign(dob);
-    char* Class=new char[30];
-    std::cout<<"Insert class: ";
-    std::cin.ignore();
-    std::cin.get(Class,'/n');
-    std::cin.ignore();
-    student.get_table().get(student.get_table().num_rows()-1,"Class").assign(Class);
-    if (student.init_write()==false){
+    Interface::input_menu menu;
+    menu.set_title("Add Student");
+    menu.add_item("Student ID: ",false);
+    menu.add_item("Fullname: ",true);
+    menu.add_item("Date of birth: ",true);
+    menu.add_item("Class: ",false);
+    list<multitype> res=menu.print_menu_and_wait(false);
+    students.get_table().get(students.get_table().num_rows()-1,"Student ID").assign(res.at(0));
+    students.get_table().get(students.get_table().num_rows()-1,"Fullname").assign(res.at(1));
+    students.get_table().get(students.get_table().num_rows()-1,"DoB").assign(res.at(2));
+    students.get_table().get(students.get_table().num_rows()-1,"Class").assign(res.at(3));
+    if (students.init_write()==false){
         std::cout<<"Error!";
         return;
     }
-    student.write_and_terminate(); //paste the table to the csv file
-    csv_handler idpass("idpass.csv");
-    if (idpass.init_read()==false){
+    students.write_and_terminate(); //paste the table to the csv file
+    csv_handler users("users.csv");
+    if (users.init_read()==false){
         std::cout<<"Error!";
         return;
     }
-    idpass.read_and_terminate();
-    idpass.get_table().add_row();
-    idpass.get_table().get(idpass.get_table().num_rows()-1,"ID").assign(student.get_table().get(student.get_table().num_rows()-1,"Student ID").to_str());
-    idpass.get_table().get(idpass.get_table().num_rows()-1,"Role").assign("Student");
+    users.read_and_terminate();
+    users.get_table().add_row();
+    users.get_table().get(users.get_table().num_rows()-1,"ID").assign(res.at(0));
+    users.get_table().get(users.get_table().num_rows()-1,"Role").assign("Student");
     char* dobb=new char[20];
     char* temp=new char[20];
-    temp=student.get_table().get(student.get_table().num_rows()-1,"DoB").to_str();
+    temp=res.at(2).to_str();
     int j=0;
     for (int i=0;i<strlen(temp);i++){
         if (temp[i]!='/'){
@@ -91,37 +94,40 @@ void addStudent(){
             j++;
         }
     }
-    idpass.get_table().get(idpass.get_table().num_rows()-1,"Pass").assign(dobb);
-    if (idpass.init_write()==false){
+    users.get_table().get(users.get_table().num_rows()-1,"Pass").assign(dobb);
+    if (users.init_write()==false){
         std::cout<<"Error!";
         return;
     }
-    idpass.write_and_terminate();
+    users.write_and_terminate();
 }
 
 void editStudent(){
     using namespace Csv;
-    csv_handler student("students.csv");
-    if (student.init_read()==false){
+    csv_handler students("students.csv");
+    if (students.init_read()==false){
         std::cout<<"Error!";
         return;
     }
-    student.read_and_terminate(); //read the csv file and copy to table
-    std::cout<<"Select the Student ID you want to edit: ";
-    int no;
-    std::cin>>no;
-    std::cout<<"Select what you want to edit: "<<std::endl;
-    std::cout<<"1. Student ID"<<std::endl;
-    std::cout<<"2. Fullname"<<std::endl;
-    std::cout<<"3. Date of birth"<<std::endl;
-    std::cout<<"4. Class"<<std::endl;
-    int num;
-    std::cin>>num;
+    students.read_and_terminate(); //read the csv file and copy to table
+    Interface::input_menu menu;
+    menu.set_title("Edit Student");
+    menu.add_item("Student ID: ",false);
+    list<multitype> res=menu.print_menu_and_wait(true);
+    Interface::select_menu menu2;
+    menu2.set_title("Which part you want to edit: ");
+    menu2.add_item(1,"Student ID");
+    menu2.add_item(2,"Fullname");
+    menu2.add_item(3,"Date of birth");
+    menu2.add_item(4,"Class");
+    int num=menu2.print_menu_and_wait().to_int();
     if (num==1){
         int id;
         std::cout<<"Insert Student ID: ";
         std::cin>>id;
-        student.get_table().get_row_where("Student ID",no).at(0).assign(id);
+        students.get_table().get_row_where("Student ID",res.at(0)).at(0).assign(id);
+        users.get_table().get_row_where("Student ID",res.at(0)).at(0).assign(id);
+        coursestudent.get_table().get_row_where("Student ID",res.at(0)).at(1).assign(id);
         std::cin.ignore();
     }
     if (num==2){
@@ -129,7 +135,7 @@ void editStudent(){
         std::cout<<"Insert fullname: ";
         std::cin.ignore();
         std::cin.get(fullname,'/n');
-        student.get_table().get_row_where("Student ID",no).at(1).assign(fullname);
+        students.get_table().get_row_where("Student ID",res.at(0)).at(1).assign(fullname);
         std::cin.ignore();
     }
     if (num==3){
@@ -137,7 +143,7 @@ void editStudent(){
         std::cout<<"Insert date of birth: ";
         std::cin.ignore();
         std::cin.get(dob,'/n');
-        student.get_table().get_row_where("Student ID",no).at(2).assign(dob);
+        students.get_table().get_row_where("Student ID",res.at(0)).at(2).assign(dob);
         std::cin.ignore();
     }
     if (num==4){
@@ -145,37 +151,37 @@ void editStudent(){
         std::cout<<"Insert class: ";
         std::cin.ignore();
         std::cin.get(Class,'/n');
-        student.get_table().get_row_where("Student ID",no).at(3).assign(Class);
+        students.get_table().get_row_where("Student ID",res.at(0)).at(3).assign(Class);
         std::cin.ignore();
     }
-    if (student.init_write()==false){
+    if (students.init_write()==false){
         std::cout<<"Error!";
         return;
     }
-    student.write_and_terminate(); //paste the table to the csv file
+    students.write_and_terminate(); //paste the table to the csv file
 }
 
-void updateIDPass(){
+void updateusers(){
     using namespace Csv;
-    csv_handler student("students.csv");
-    if (student.init_read()==false){
+    csv_handler students("students.csv");
+    if (students.init_read()==false){
         std::cout<<"Error!";
         return;
     }
-    student.read_and_terminate();
-    for (int i=0;i<student.get_table().num_rows();i++){
-        csv_handler idpass("idpass.csv");
-        if (idpass.init_read()==false){
+    students.read_and_terminate();
+    for (int i=0;i<students.get_table().num_rows();i++){
+        csv_handler users("users.csv");
+        if (users.init_read()==false){
             std::cout<<"Error!";
             return;
         }
-        idpass.read_and_terminate();
-        idpass.get_table().add_row();
-        idpass.get_table().get(idpass.get_table().num_rows()-1,"ID").assign(student.get_table().get(i,"Student ID").to_str());
-        idpass.get_table().get(idpass.get_table().num_rows()-1,"Role").assign("Student");
+        users.read_and_terminate();
+        users.get_table().add_row();
+        users.get_table().get(users.get_table().num_rows()-1,"ID").assign(students.get_table().get(i,"Student ID").to_str());
+        users.get_table().get(users.get_table().num_rows()-1,"Role").assign("Student");
         char* dob=new char[20];
         char* temp=new char[20];
-        temp=student.get_table().get(i,"DoB").to_str();
+        temp=students.get_table().get(i,"DoB").to_str();
         int j=0;
         for (int i=0;i<strlen(temp);i++){
             if (temp[i]!='/'){
@@ -183,12 +189,12 @@ void updateIDPass(){
                 j++;
             }
         }
-        idpass.get_table().get(idpass.get_table().num_rows()-1,"Pass").assign(dob);
-        if (idpass.init_write()==false){
+        users.get_table().get(users.get_table().num_rows()-1,"Pass").assign(dob);
+        if (users.init_write()==false){
             std::cout<<"Error!";
             return;
         }
-        idpass.write_and_terminate();
+        users.write_and_terminate();
     }
 }
 
@@ -206,32 +212,34 @@ bool filterClass (multitype column,list<multitype> row,table tb){
     return true;
 }
 
+multitype s;
 bool filterStudent (multitype column,list<multitype> row,table tb){
     if (!column.equal("Fullname"))
         return false;
-    return row.at(3).equal("19APCS2");
+    return row.at(3).equal(s);
 }
 
 void viewClass(){
     using namespace Csv;
-    csv_handler student("students.csv");
-    if (student.init_read()==false){
+    csv_handler students("students.csv");
+    if (students.init_read()==false){
         std::cout<<"Error!";
         return;
     }
-    student.read_and_terminate();
-    Interface::print_table(student.get_table().filter(filterClass),"Class");
+    students.read_and_terminate();
+    Interface::print_table(students.get_table().filter(filterClass),"Class");
 }
 
 void viewStudentInClass(){
     using namespace Csv;
-    csv_handler student("students.csv");
-    if (student.init_read()==false){
+    csv_handler students("students.csv");
+    if (students.init_read()==false){
         std::cout<<"Error!";
         return;
     }
-    student.read_and_terminate();
-    Interface::print_table(student.get_table().filter(filterStudent),"Students");
+    students.read_and_terminate();
+    std::cin>>s;
+    Interface::print_table(students.get_table().filter(filterStudent),"Students");
 }
 
 }
